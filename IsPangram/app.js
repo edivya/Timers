123d0d7fef9a4553c79f89dc13f45813cec74e24
Divya <enjamuridivya@gmail.com>
2019-07-20 23:24:08 -0700

    modified program IsPangram

--- IsPangram/app.js
+++ IsPangram/app.js
@@ -1,32 +1,54 @@
-var test1 = "The quick brown fox jumps over the lazy dog.";
-
-var isPangram = function(string) {
-
-  // If the string contains less than 26 letters, it couldn't have all 26 letters of the alphabet
-  if (string.length < 26) {
-    return false;
-  }
-  else {
-
-    // Holder for the unique letters found in the sentence
-    var letterHolder = [];
-
-    // Make the string lowercase, remove anything that isn't the letters a through z, then split them into an array
-    var letters = string.toLowerCase().replace(/[^a-z]+/g, "").split("");
-
-    // loop over the letters, if a letter is not found in the letterHolder array, push it in
-    for (var i = 0; i < letters.length; i++) {
-      if (letterHolder.indexOf(letters[i]) === -1) {
-        letterHolder.push(letters[i]);
-      }
+var countLetters = function(s){
+    var letters = {};
+    for(var i = 0; i < s.length; i++){
+      var letter = s[i];
+      if(letter == " ") continue;
+      letters[letter] = (letters[letter] || 0) + 1;
     }
-
-    // Check to see if letterHolder.length contains the 26 letters of the alphabet.
-    if (letterHolder.length === 26) {
-      return true;
-    }
-    else {
+    return letters;
+  };
+  
+  var isAnagram = function(s1, s2){
+    s1 = s1.toLowerCase().trim();
+    s2 = s2.toLowerCase().trim();
+  
+    if(s1 === s2){
       return false;
     }
-  }
-};
+  
+    return _.eq(countLetters(s1), countLetters(s2));
+  };
+  
+  // Для тех кто не хочет подключать lodash
+  // http://stackoverflow.com/questions/1068834/object-comparison-in-javascript
+  var _ = {
+    eq: function(a,b){
+      for(var i in a) {
+        if(b[i] != a[i]) return false;
+      }
+      for(var i in b) {
+        if(b[i] != a[i]) return false;
+      }
+      return true;
+    }
+  
+  };
+  
+  
+  console.log(isAnagram("стационар", "соратница"));
+  console.log(isAnagram("покраснение", "пенсионерка"));
+  console.log(isAnagram("внимание", "Вениамин"));
+  console.log(isAnagram("апельсин", "спаниель"));
+  console.log(isAnagram("eleven plus two", "twelve plus one"));
+  console.log(isAnagram("statue of liberty", "built to stay free"));
+  console.log(isAnagram("older and wiser", "I learned words"));
+  
+  // false
+  console.log(isAnagram("стационар", "стационар"));
+  console.log(isAnagram("стационар", "сортница"));
+  console.log(isAnagram("покранение", "пенсионерка"));
+  console.log(isAnagram("внимание", "Вениамина"));
+  console.log(isAnagram("eleven plus one", "спаниель"));
+  console.log(isAnagram("eleven plus one", "twelve plus one"));
+  console.log(isAnagram("statue of paris", "built to stay free"));
+  console.log(isAnagram("older and wiser", "you learned words"));
